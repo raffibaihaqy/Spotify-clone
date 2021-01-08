c4287f88d000308fff1efe519d90835260a7b6f4
Zero Blitz <raffibaihaqy02@gmail.com>
2021-01-08 18:42:51 +0700

    Login & Regis Auth

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import LoadingScreen from './screens/LoadingScreen'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import firebase from 'firebase'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+var firebaseConfig = {
+  apiKey: "AIzaSyDBFrgUmw5J3nXPBQLCSC639AzjlTC60yo",
+  authDomain: "spotifyclone-bd150.firebaseapp.com",
+  projectId: "spotifyclone-bd150",
+  storageBucket: "spotifyclone-bd150.appspot.com",
+  messagingSenderId: "347665604604",
+  appId: "1:347665604604:web:62b7c3edc3bbb2d0281c79"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen
 });
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+);
---
+++ screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, Touchable } from "react-native";
+import firebase from 'firebase'
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default class HomeScreen extends React.Component {
+    state = {
+        email: "",
+        displayName: ""
+    }
+
+    componentDidMount() {
+        const {email, displayName} = firebase.auth().currentUser
+
+        this.setState({email, displayName});
+    }
+
+    signOutUser = () => {
+        firebase.auth().signOut();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hi {this.state.email}!</Text>
+                
+                <TouchableOpacity style={{marginTop: 32}} onPress={this.signOutUser}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
